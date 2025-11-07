7b48792f296cf5f5488e7782b2c7779b43b3a79c
Anthony-Villamar <anthonyvillamar3000@gmail.com>
2025-11-07 02:38:25 -0500

    verson 2

--- routes/db.js
+++ routes/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool =  mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'A0957333255a',
   database: process.env.DB_NAME || 'san_jose_encuestas',
   port: process.env.DB_PORT || 3306,
   timezone: '-05:00', // 🇪🇨 Ecuador
